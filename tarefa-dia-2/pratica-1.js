9ef4416f89e235f1b950fe5022bc0406d286cb9f
Leonardo Calsavara <calsavara2511@gmail.com>
2022-08-24 11:18:35 -0300

    atribuicao de variaveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,6 @@
-let nome
-let mail
-let job
-let old 
-let dream
+let nome = "Leonardo Calsavara"
+let mail = 'calsavara2511@gmail.com'
+let job = "Estudante"
+let old = 20
+let dream = "somewhere"
+
